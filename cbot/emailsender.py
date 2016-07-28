c710bfd0c9d42cb2fbf58dc160223783aa258d3d
Alexander Garvin <alexwgarvin@gmail.com>
2016-07-27 22:12:55 -0400

    merging...

--- cbot/emailsender.py
+++ cbot/emailsender.py
@@ -8,48 +8,49 @@ from email.MIMEImage import MIMEImage
 '''User:craigslistpythonproject@gmail.com'''
 '''Pass:Programmingisfun1'''
 if __name__ == "__main__":
-	sender = "craigslistpythonproject@gmail.com"  #our email
-	reciever = "craigslistpythonproject@gmail.com"#recipients email
-	cellphone = "7179915983@vtext.com"#need to format according to carrier
-	
-	#format multipart message
-	message = MIMEMultipart('related')
-	message['From'] = sender
-	message['To'] = reciever
-	message['Subject'] = "Craigslist Item"
+    sender = "craigslistpythonproject@gmail.com"  #our email
+    # reciever = "craigslistpythonproject@gmail.com"#recipients email
+    reciever = "alexwgarvin@gmail.com"
+    cellphone = "7179915983@vtext.com"#need to format according to carrier
+    
+    #format multipart message
+    message = MIMEMultipart('related')
+    message['From'] = sender
+    message['To'] = reciever
+    message['Subject'] = "Craigslist Item"
 
-	#format multipart message
-	textmessage = MIMEMultipart('related')
-	textmessage['From'] = sender
-	textmessage['To'] = reciever
-	textmessage['Subject'] = ""
+    #format multipart message
+    textmessage = MIMEMultipart('related')
+    textmessage['From'] = sender
+    textmessage['To'] = reciever
+    textmessage['Subject'] = ""
 
-	#email message text
-	altmessage = MIMEMultipart('alternative')
-	message.attach(altmessage)
-	body = '<b>Your <i>Item</i> has been found!</b><br><img src="cid:image1"><br> Look a pic!'
-	altmessage.attach(MIMEText(body, 'html'))
+    #email message text
+    altmessage = MIMEMultipart('alternative')
+    message.attach(altmessage)
+    body = '<b>Your <i>Item</i> has been found!</b><br><img src="cid:image1"><br> Look a pic!'
+    altmessage.attach(MIMEText(body, 'html'))
 
-	#text message text
-	textbody = 'An e-mail has been sent to you regarding your craigslist search!'
-	textmessage.attach(MIMEText(textbody, 'plain'))
+    #text message text
+    textbody = 'An e-mail has been sent to you regarding your craigslist search!'
+    textmessage.attach(MIMEText(textbody, 'plain'))
 
-	#open, save and close image
-	fp = open('test.jpg', 'rb')
-	messageimage = MIMEImage(fp.read())
-	fp.close()
+    #open, save and close image
+    fp = open('test.jpg', 'rb')
+    messageimage = MIMEImage(fp.read())
+    fp.close()
 
-	#attach image to message
-	messageimage.add_header('Content-ID', '<image1>')
-	message.attach(messageimage)
+    #attach image to message
+    messageimage.add_header('Content-ID', '<image1>')
+    message.attach(messageimage)
 
-	#connect to server and send email and text
-	server = smtplib.SMTP('smtp.gmail.com', 587)#need to change according to mail service
-	server.starttls()
-	server.login(sender, "Programmingisfun1") 
-	server.sendmail(sender, reciever, message.as_string())
-	server.sendmail(sender, cellphone, textmessage.as_string())
-	server.quit()
+    #connect to server and send email and text
+    server = smtplib.SMTP('smtp.gmail.com', 587)#need to change according to mail service
+    server.starttls()
+    server.login(sender, "Programmingisfun1") 
+    server.sendmail(sender, reciever, message.as_string())
+    server.sendmail(sender, cellphone, textmessage.as_string())
+    server.quit()
 
 
 
--- cbot/searchitem.py
+++ cbot/searchitem.py
@@ -7,66 +7,86 @@ craigUrl = "craigslist.org"
 class craigList:
     def __init__(self, sRegion, sCatagory, sKeyword, sMinDate):
         self.pageLinks = []
+        self.dateTables = []
         self.cList = []
         self.sCit = "http://" + sRegion + "."
         self.sCat = sCatagory
         self.sKey = "?query=" + sKeyword
         self.MinD = sMinDate
         self.parseLinks()
-        self.parsePage()
+        self.minY = int(sMinDate[:4])
+        self.minM = int(sMinDate[5:7])
+        self.minD = int(sMinDate[8:])
+        if self.pageLinks == []:
+            pass
+        else:
+            self.parsePage()
     def parseLinks(self):
-        # print searchIndex.items()
         for k,v in searchIndex.items():
-            # print self.sCat
-            # print k
-            # print v
             if self.sCat in k:
                 pagewanted = v
-                # print "FOUND IT"
                 break
-            # else:
-            #     sys.stderr.write ("page not found\n")
-            #     return
         pageNum = 0
-        url = self.sCit + pagewanted + self.sKey
+
         while True:
+            url = self.sCit + pagewanted + self.sKey
             if pageNum == 0:
                 pass
             else:
-                url = self.sCit + pagewanted + "?s=" + str(pageNum)
+                url += "&s=" + str(pageNum)
+            url += "&sort=date"
+            #print url
             pageNum += 100
             cPage = requests.get(url)
             pageTree = html.fromstring(cPage.text)
             pageTable = pageTree.xpath("//a[@class='hdrlnk']/@href")
-            if len(pageTable) < 10:
+            dateTable = pageTree.xpath("//span[@class='pl']/time/@datetime")
+            #print pageTable, len(pageTable)
+            #print dateTable, len(dateTable)
+            if len(pageTable) < 10 or pageTable == []:
                 break
             self.pageLinks.append(pageTable)
+            self.dateTables.append(dateTable)
+            pageTable = []
     def parsePage(self):
-        control = 0
+        x = 0
+        y = 0
         for pageLink in self.pageLinks:
             for searchitem in pageLink:
-                #print searchitem
                 correctUrl = self.sCit + craigUrl + searchitem
                 if craigUrl in searchitem: break
-                cItem = craigItem(correctUrl, self.sCit, self.MinD)
-                #print cItem.getDate()
-                if 's' in cItem.getDate() or 'N' in cItem.getDate():
-                    control += 1
+                testDate = self.dateTables[x][y]
+                testD = testDate[:10]
+                Y = int(testD[:4])
+                M = int(testD[5:7])
+                D = int(testD[8:])
+                dateval = lambda y, m, d: y * 365 + m*30 + d
+                if dateval(Y, M, D) < dateval(self.minY, self.minM, self.minD):
+                    return
+                cItem = craigItem(correctUrl, self.sCit, self.MinD, testD)
+                if "none" in cItem.getTitle():
                     continue
+                if 's' in cItem.getDate() or 'N' in cItem.getDate():
+                    pass
+                    #continue
                 self.cList.append(cItem)
-                if control == 10:
-                   break
-                control += 1
-            break
+                y += 1
+            x += 1
 
 class craigItem:
-    def __init__(self, cPag, city, minDate):
+    def __init__(self, cPag, city, minDate, upDte):
         self.minY = int(minDate[:4])
         self.minM = int(minDate[5:7])
         self.minD = int(minDate[8:])
+        self.upDate = upDte
         self.cityurl = city
         iUrl = str(cPag)
-        itemPage = requests.get(iUrl)
+        #print iUrl
+        try:
+            itemPage = requests.get(iUrl)
+        except:
+            self.title = ["none"]
+            return
         self.itemPageTree = html.fromstring(itemPage.text)
         self.setTitle()
         self.setPrice()
@@ -74,17 +94,19 @@ class craigItem:
         self.setContact()
         self.setDate()
     def getTitle(self):
-        return str("".join(self.title))
+        return str("".join(str(self.title)))
     def getPrice(self):
-        return str("".join(self.price))
+        return str("".join(str(self.price)))
     def getDescr(self):
-        return str("".join(self.descr))
+        return str("".join(str(self.descr)))
     def getEmail(self):
-        return str("".join(self.email))
+        return str("".join(str(self.email)))
     def getPhone(self):
-        return str("".join(self.phone))
+        return str("".join(str(self.phone)))
     def getDate(self):
         return str(self.date[0])
+    def getUpDate(self):
+        return str(self.upDate)
     def setTitle(self):
         try:
             self.title = self.itemPageTree.xpath(
@@ -99,7 +121,7 @@ class craigItem:
             self.price = self.itemPageTree.xpath(
                     "//span[@class='postingtitletext']/span[@class='price']//text()")
         except:
-            self.price = "Not Available"
+            self.price = ["Not Available"]
         while '\n' in self.price: self.price.remove('\n')
         if self.price == []:
             self.price = ["Not Available"]
@@ -108,13 +130,17 @@ class craigItem:
             self.descr = self.itemPageTree.xpath(
                     "//section[@id='postingbody']//text()")
         except:
-            self.descr = 'Not Available'
+            self.descr = ['Not Available']
         while '\n' in self.descr: self.descr.remove('\n')
         if self.descr == []:
             self.descr = ["Not Available"]
     def setContact(self):
         replyLink = self.itemPageTree.xpath(
                 "//a[@id='replylink']/@href")
+        if replyLink == []:
+            self.phone = ['Not Available']
+            self.email = ['Not Available']
+            return
         contactUrl = self.cityurl + craigUrl + replyLink[0]
         contactPage = requests.get(contactUrl)
         contactTree = html.fromstring(contactPage.text)
@@ -122,12 +148,12 @@ class craigItem:
             self.phone = contactTree.xpath(
                     "//ul/li/text()")
         except:
-            self.phone = 'Not Available'
+            self.phone = ['Not Available']
         try:
             self.email = contactTree.xpath(
                     "//div[@class='anonemail']//text()")
         except:
-            self.email = 'Not Available'
+            self.email = ['Not Available']
         while '\n' in self.email: self.email.remove('\n')
         count = 0
         for entry in self.phone:
@@ -139,7 +165,7 @@ class craigItem:
             count += 1
         if self.email == []:
             self.email = ["Not Available"]
-        if len(self.phone) > 17:
+        if len(self.phone) > 17 or self.phone == []:
             self.phone = ["Not Available"]
     def setDate(self):
         try:
@@ -152,29 +178,29 @@ class craigItem:
             D = int(self.date[0][8:])
             dateval = lambda y, m, d: y * 365 + m*30 + d
             if dateval(Y, M, D) < dateval(self.minY, self.minM, self.minD):
-                self.date[0] = ['stop']
-            # print self.date[0]
+                pass
+                #self.date[0] = ['stop']
         except:
-            self.date = 'Not Available'
+            self.date = ['Not Available']
         while '\n' in self.date: self.date.remove('\n')
         if self.date == []:
             self.date = ["Not Available"]
-    def __str__(self):
-        returnthis = "title:\t" + "".join(self.title)\
-        +"\nprice:\t" + "".join(self.price)\
-        +"\ndescr:\t" + "".join(self.descr)\
-        +"\nemail:\t" + "".join(self.email)\
-        +"\nphone:\t" + "".join(self.phone)\
-        +"\ndate:\t" + self.date[0]
-        return returnthis.encode('utf-8')
+#     def __str__(self):
+#         returnthis = "title:\t" + "".join(self.title)\
+#         +"\nprice:\t" + "".join(self.price))\
+#         +"\ndescr:\t" + "".join(self.descr))\
+#         +"\nemail:\t" + "".join(self.email))\
+#         +"\nphone:\t" + "".join((self.phone))\
+#         +"\ndate:\t" + self.date[0]\
+#         +"\nupDate:\t" + self.upDate
+#         return returnthis.encode('utf-8')
 
-if __name__ == "__main__":
-    c = craigList("tallahassee","antiques","chair", "2016-07-10")
-    number = 1
-    print "***********************************************"
-    for it in c.cList:
-        print number
-        print it
-        print "***********************************************"
-        number += 1
-
+# if __name__ == "__main__":
+#     c = craigList("tallahassee","antiques","chair", "2016-07-05")
+#     number = 1
+#     print "***********************************************"
+#     for it in c.cList:
+#         print number
+#         print it
+#         print "***********************************************"
+#         number += 1
